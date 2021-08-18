4bf86bb8220082c78d5a25ed698683475751b6f4
Felipe Cardozo <fraifelipe@gmail.com>
2021-08-18 00:52:08 -0300

    1.2 check permutation

---
+++ 01-strings/1.2-check-permutation/permutation.go
@@ -0,0 +1,36 @@
+package permutation
+
+import (
+	"github.com/fraidev/cracking-coding/utils"
+)
+
+func IsPermutation(str, input string) bool {
+
+	if len(str) != len(input) {
+		return false
+	}
+
+	return utils.SortString(str) == utils.SortString(input)
+}
+
+func IsPermutation2(str, input string) bool {
+
+	if len(str) != len(input) {
+		return false
+	}
+
+	letters := make(map[rune]int, 128)
+
+	for _, r := range str {
+		letters[r]++
+	}
+
+	for _, r := range input {
+		letters[r]--
+		if letters[r] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ 01-strings/1.2-check-permutation/permutation_test.go
@@ -0,0 +1,28 @@
+package permutation
+
+import "testing"
+
+func TestIsPermutation(t *testing.T) {
+	type args struct {
+		str   string
+		input string
+	}
+	tests := []struct {
+		args args
+		want bool
+	}{
+		{args: args{str: "ab", input: "dc"}, want: false},
+		{args: args{str: "ab", input: "ba"}, want: true},
+		{args: args{str: "abcce", input: "cabec"}, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.args.str+" and "+tt.args.input, func(t *testing.T) {
+			if got := IsPermutation(tt.args.str, tt.args.input); got != tt.want {
+				t.Errorf("IsPermutation() = %v, want %v", got, tt.want)
+			}
+			if got := IsPermutation2(tt.args.str, tt.args.input); got != tt.want {
+				t.Errorf("IsPermutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
