f112f543ae2c6189b954287afb835c8bc4fe2ff8
Felipe Cardozo <fraifelipe@gmail.com>
2021-08-26 23:05:30 -0300

    1.4 palindrome permutation

---
+++ 01-strings/1.4.palindrome-permutation/palindromepermutation.go
@@ -0,0 +1,26 @@
+package palindromepermutation
+
+import (
+	"strings"
+)
+
+func Validate(input string) bool {
+	str := strings.ReplaceAll(strings.ToLower(input), " ", "")
+	hashMap := map[rune]int{}
+	alreadyHasMiddleChar := false
+
+	for _, r := range str {
+		hashMap[r]++
+	}
+
+	for _, m := range hashMap {
+		if m%2 == 1 {
+			if alreadyHasMiddleChar {
+				return false
+			}
+			alreadyHasMiddleChar = true
+		}
+	}
+
+	return true
+}
---
+++ 01-strings/1.4.palindrome-permutation/palindromepermutation_test.go
@@ -0,0 +1,24 @@
+package palindromepermutation
+
+import "testing"
+
+func TestValidate(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		args args
+		want bool
+	}{
+		{args: args{str: "Tact Coa"}, want: true},
+		{args: args{str: "Tact boa"}, want: false},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.args.str, func(t *testing.T) {
+			if got := Validate(tt.args.str); got != tt.want {
+				t.Errorf("Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
